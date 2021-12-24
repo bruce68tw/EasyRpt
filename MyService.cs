5eacec5b9b1e42519ec2e7f8d852c77a3bf75b9e
Bruce Chen <bruce68tw@gmail.com>
2021-12-24 20:04:44 +0800

    initial

---
+++ MyService.cs
@@ -0,0 +1,75 @@
+﻿using Base.Enums;
+using Base.Models;
+using Base.Services;
+using System.IO;
+using System.Net.Mail;
+using System.Net.Mime;
+using System.Threading.Tasks;
+
+namespace EasyRpt
+{
+    public class MyService
+    {
+        public async Task RunAsync()
+        {
+            const string preLog = "EasyRpt: ";
+            await _Log.InfoAsync(preLog + "Start.");
+
+            #region 1.read XpEasyRpt rows
+            var info = "";
+            var db = new Db();
+            var rpts = await db.GetJsonsAsync("select * from dbo.XpEasyRpt where Status=1");
+            if (rpts == null)
+            {
+                info = "No XpEasyRpt Rows";
+                goto lab_exit;
+            }
+            #endregion
+
+            //send report loog
+            var smtp = _Fun.Smtp;
+            foreach (var rpt in rpts)
+            {
+                #region 2.set mailMessage
+                var rptName = rpt["Name"].ToString();
+                var email = new EmailDto()
+                {
+                    Subject = rptName,
+                    ToUsers = _Str.ToList(rpt["ToEmails"].ToString()),
+                    Body = "Hello, please check attached report.",
+                };
+                var msg = _Email.DtoToMsg(email, smtp);
+                #endregion
+
+                //3.sql to Memory Stream docx
+                var ms = new MemoryStream();
+                var docx = _Excel.FileToMsDocx(_Fun.DirRoot + "EasyRptData/" + rpt["TplFile"].ToString(), ms); //ms <-> docx
+                await _Excel.DocxBySqlAsync(rpt["Sql"].ToString(), docx, 1, db);
+                docx.Dispose(); //must dispose, or get empty excel !!
+
+                //4.set attachment
+                ms.Position = 0;
+                Attachment attach = new Attachment(ms, new ContentType(ContentTypeEstr.Excel));
+                attach.Name = rptName + ".xlsx";
+                msg.Attachments.Add(attach);
+
+                //5.send email
+                await _Email.SendByMsgAsync(msg, smtp);    //sync send for stream attachment !!
+                ms.Close(); //close after send email, or get error: cannot access a closed stream !!
+
+                await _Log.InfoAsync(preLog + "Send " + rptName);
+            }
+
+            #region 6.close db & log
+        lab_exit:
+            if (db != null)
+                await db.DisposeAsync();
+            if (info != "")
+                await _Log.InfoAsync(preLog + info);
+
+            await _Log.InfoAsync(preLog + "End.");
+            #endregion
+        }
+
+    }//class
+}
---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using Base.Enums;
+using Base.Models;
+using Base.Services;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Data.SqlClient;
+using System.Threading.Tasks;
+
+namespace EasyRpt
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            //1.initial & load EasyRptConfig.json
+            IConfiguration configBuild = new ConfigurationBuilder()
+                .AddJsonFile("EasyRptConfig.json", optional: true, reloadOnChange: true)
+                .Build();
+
+            //2.appSettings "FunConfig" section -> _Fun.Config
+            var config = new ConfigDto();
+            configBuild.GetSection("FunConfig").Bind(config);
+            _Fun.Config = config;
+
+            //3.setup our DI
+            var services = new ServiceCollection();
+
+            //4.base user info for base component
+            services.AddSingleton<IBaseUserService, BaseUserService>();
+
+            //5.ado.net for mssql
+            services.AddTransient<DbConnection, SqlConnection>();
+            services.AddTransient<DbCommand, SqlCommand>();
+
+            //6.initial _Fun by mssql
+            IServiceProvider diBox = services.BuildServiceProvider();
+            _Fun.Init(false, diBox);
+
+            //7.run main 
+            await new MyService().RunAsync();
+        }
+    }
+}
